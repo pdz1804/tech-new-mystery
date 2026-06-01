03fd1fab7d815d8468aa59e25b331792f9935f28
Developer <developer@local>
2026-06-01 09:15:28 +0700

    fix: Allow bedrock-agentcore InvokeAgentRuntime on both base runtime ARN and endpoint paths

--- infra/terraform/iam.tf
+++ infra/terraform/iam.tf
@@ -254,7 +254,10 @@ resource "aws_iam_role_policy" "backend_invoke_agentcore" {
       Sid      = "InvokeAgentRuntime"
       Effect   = "Allow"
       Action   = ["bedrock-agentcore:InvokeAgentRuntime"]
-      Resource = "${aws_bedrockagentcore_agent_runtime.agent_core.agent_runtime_arn}/*"
+      Resource = [
+        aws_bedrockagentcore_agent_runtime.agent_core.agent_runtime_arn,
+        "${aws_bedrockagentcore_agent_runtime.agent_core.agent_runtime_arn}/*"
+      ]
     }]
   })
 }
